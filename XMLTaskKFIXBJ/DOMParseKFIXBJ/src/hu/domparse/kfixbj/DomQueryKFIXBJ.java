acae490a0116460081b7d9459a7b86c173f9d242
Molnár Balázs <molbazsa@gmail.com>
2022-11-29 16:20:10 +0100

    XML Task (2) Update DOM Query

--- XMLTaskKFIXBJ/DOMParseKFIXBJ/src/hu/domparse/kfixbj/DomQueryKFIXBJ.java
+++ XMLTaskKFIXBJ/DOMParseKFIXBJ/src/hu/domparse/kfixbj/DomQueryKFIXBJ.java
@@ -2,12 +2,16 @@ package hu.domparse.kfixbj;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import org.xml.sax.SAXException;
@@ -21,6 +25,15 @@ public class DomQueryKFIXBJ {
 
         // Mely vonat érint adott állomást
         query2(doc, "a3");
+
+        // Adott állomást mely napokon érint vonat
+        query3(doc, "a2");
+
+        // Adott vonat menetideje
+        query4(doc, "v3");
+
+        // Állomások, amelyek 165 km-nél messzebb vannak
+        query5(doc);
     }
 
     public static void query1(Document doc, String jid) {
@@ -28,27 +41,27 @@ public class DomQueryKFIXBJ {
 
         Element jegy = getElementById(doc, "jegy", "jid", jid);
 
-        String uid = jegy.getElementsByTagName("utas").item(0).getTextContent();
+        String uid = firstChildTextContent(jegy, "utas");
         Element utas = getElementById(doc, "utas", "uid", uid);
-        String utasNev = utas.getElementsByTagName("nev").item(0).getTextContent();
+        String utasNev = firstChildTextContent(utas, "nev");
 
-        String aid1 = jegy.getElementsByTagName("allomas1").item(0).getTextContent();
-        String aid2 = jegy.getElementsByTagName("allomas2").item(0).getTextContent();
+        String aid1 = firstChildTextContent(jegy, "allomas1");
+        String aid2 = firstChildTextContent(jegy, "allomas2");
         Element allomas1 = getElementById(doc, "allomas", "aid", aid1);
         Element allomas2 = getElementById(doc, "allomas", "aid", aid2);
-        String allomasNev1 = allomas1.getElementsByTagName("nev").item(0).getTextContent();
-        String allomasNev2 = allomas2.getElementsByTagName("nev").item(0).getTextContent();
+        String allomasNev1 = firstChildTextContent(allomas1, "nev");
+        String allomasNev2 = firstChildTextContent(allomas2, "nev");
 
         System.out.printf("%s (ID: %s) utazása:%n", utasNev, uid);
-        System.out.printf("%sIndulási állomás:%s%n", indent, allomasNev1);
-        System.out.printf("%sÉrkezési állomás:%s%n", indent, allomasNev2);
+        System.out.printf("%sIndulási állomás: %s%n", indent, allomasNev1);
+        System.out.printf("%sÉrkezési állomás: %s%n", indent, allomasNev2);
 
         System.out.println();
     }
 
     public static void query2(Document doc, String aid) {
         Element allomas = getElementById(doc, "allomas", "aid", aid);
-        String allomasNev = allomas.getElementsByTagName("nev").item(0).getTextContent();
+        String allomasNev = firstChildTextContent(allomas, "nev");
 
         NodeList erintiElements = doc.getElementsByTagName("erinti");
 
@@ -57,13 +70,12 @@ public class DomQueryKFIXBJ {
 
             if (erinti.getAttribute("aid").equals(aid)) {
                 String vid = erinti.getAttribute("vid");
-                boolean vegallomas = Boolean.parseBoolean(
-                        erinti.getElementsByTagName("vegallomas").item(0).getTextContent()
-                );
+                boolean vegallomas = Boolean.parseBoolean(firstChildTextContent(erinti, "vegallomas"));
+
                 System.out.printf(
-                        "%s vonat érinti %s állomást %s%n",
-                        vid, allomasNev,
-                        vegallomas ? "(végállomás)" : ""
+                    "%s vonat érinti %s állomást %s%n",
+                    vid, allomasNev,
+                    vegallomas ? "(végállomás)" : ""
                 );
             }
         }
@@ -71,21 +83,91 @@ public class DomQueryKFIXBJ {
         System.out.println();
     }
 
-    public static Element getElementById(Document doc, String tagName, String attribute, String id) {
-        NodeList nodeList = doc.getDocumentElement().getChildNodes();
+    public static void query3(Document doc, String aid) {
+        Element allomas = getElementById(doc, "allomas", "aid", aid);
+        String allomasNev = firstChildTextContent(allomas, "nev");
 
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node node = nodeList.item(i);
+        List<LocalDate> dates = new ArrayList<>();
+        NodeList erintiElements = doc.getElementsByTagName("erinti");
 
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-                Element elem = (Element) node;
+        for (int i = 0; i < erintiElements.getLength(); i++) {
+            Element erinti = (Element) erintiElements.item(i);
 
-                if (elem.getTagName().equals(tagName) && elem.getAttribute(attribute).equals(id)) {
-                    return elem;
+            if (erinti.getAttribute("aid").equals(aid)) {
+                String vid = erinti.getAttribute("vid");
+                Element vonat = getElementById(doc, "vonat", "vid", vid);
+                LocalDateTime dateTime = LocalDateTime.parse(firstChildTextContent(vonat, "indulas"));
+                LocalDate date = dateTime.toLocalDate();
+
+                if (!dates.contains(date)) {
+                    dates.add(date);
+                    System.out.printf(
+                        "%s állomást érinti vonat %tY.%tm.%td. napon%n",
+                        allomasNev, date, date, date
+                    );
                 }
             }
         }
 
+        System.out.println();
+    }
+
+    public static void query4(Document doc, String vid) {
+        Element vonat = getElementById(doc, "vonat", "vid", vid);
+
+        LocalDateTime indulas = LocalDateTime.parse(firstChildTextContent(vonat, "indulas"));
+        LocalDateTime erkezes = LocalDateTime.parse(firstChildTextContent(vonat, "erkezes"));
+        Duration menetido = Duration.between(indulas, erkezes);
+
+        System.out.printf(
+            "%s vonat menetideje: %02d:%02d:%02d%n",
+            vid, menetido.toHoursPart(), menetido.toMinutesPart(), menetido.toSecondsPart()
+        );
+
+        System.out.println();
+    }
+
+    public static void query5(Document doc) {
+        NodeList tavolsagaElements = doc.getElementsByTagName("tavolsaga");
+
+        for (int i = 0; i < tavolsagaElements.getLength(); i++) {
+            Element tavolsaga = (Element) tavolsagaElements.item(i);
+            int tavolsag = Integer.parseInt(firstChildTextContent(tavolsaga, "tavolsag"));
+
+            if (tavolsag < 165) {
+                String aid1 = tavolsaga.getAttribute("aid1");
+                String aid2 = tavolsaga.getAttribute("aid2");
+                Element allomas1 = getElementById(doc, "allomas", "aid", aid1);
+                Element allomas2 = getElementById(doc, "allomas", "aid", aid2);
+                String allomasNev1 = firstChildTextContent(allomas1, "nev");
+                String allomasNev2 = firstChildTextContent(allomas2, "nev");
+
+                System.out.printf("%s - %s: %d km%n", allomasNev1, allomasNev2, tavolsag);
+            }
+        }
+
+        System.out.println();
+    }
+
+    public static Element getElementById(Document doc, String tagName, String attribute, String id) {
+        NodeList elements = doc.getElementsByTagName(tagName);
+
+        for (int i = 0; i < elements.getLength(); i++) {
+            Element elem = (Element) elements.item(i);
+
+            if (elem.getAttribute(attribute).equals(id)) {
+                return elem;
+            }
+        }
+
         return null;
+    }
+
+    public static Element firstChild(Element elem, String tagName) {
+        return (Element) elem.getElementsByTagName(tagName).item(0);
+    }
+
+    public static String firstChildTextContent(Element elem, String tagName) {
+        return firstChild(elem, tagName).getTextContent();
     }
 }
